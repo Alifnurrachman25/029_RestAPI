6ceff30fa45cca3692cc2b153199e0f1535deb69
Alif Nur Rachman <alif.nur.ft21@mail.umy.ac.id>
2023-12-26 17:30:38 +0700

    menambahkan TopAppBarKontak

---
+++ app/src/main/java/com/example/consumeapi/model/Kontak.kt
@@ -0,0 +1,14 @@
+package com.example.consumeapi.model
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Kontak(
+    @SerialName(value = "id")
+    val Id: Int,
+    @SerialName(value = "nama")
+    val nama: String,
+    val email: String,
+    val nohp: String
+)
